214b05433dac4ed1bf821de54d17281c53e405a4
Erin Davis <66889242+ErinNDavis@users.noreply.github.com>
2021-08-21 15:24:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import Home from './components/home';
 import Deposit from './components/deposit';
 import Withdraw from './components/withdraw';
 import AllData from './components/alldata';
-import React from 'react';
+import DataContext from './Context';
 
 function App() {
   return (
-    <HashRouter>
-      <NavBar/>
-       
-          <div className="container" style={{padding: "20px"}}>
-            <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" component={CreateAccount} />
-            <Route path="/deposit/" component={Deposit} />
-            <Route path="/withdraw/" component={Withdraw} />
-            <Route path="/alldata/" component={AllData} />
-          </div>
-       
-    </HashRouter>
+      
+      <HashRouter>
+        <NavBar/>
+        <DataContext.Provider value={{users:'peter', email:'peter@mit.edu', password:'secret', balance:100}}>
+            <div className="container" style={{padding: "20px"}}>
+              <Route path="/" exact component={Home} />
+              <Route path="/CreateAccount/" component={CreateAccount} />
+              <Route path="/deposit/" component={Deposit} />
+              <Route path="/withdraw/" component={Withdraw} />
+              <Route path="/alldata/" component={AllData} />
+            </div>
+          </DataContext.Provider>
+        </HashRouter>
+    
   );
 }
 
